0867c80ea48f387de258c29088bf3b5a6f4ff20c
Ivo Lima Jr <ivo.sl.jr@gmail.com>
2021-12-15 17:54:29 -0300

    Uso de camera para ler qrcode

---
+++ AppShopping/AppShopping/AppShopping/ViewModels/TicketScanViewModel.cs
@@ -0,0 +1,72 @@
+﻿using AppShopping.Libraries.Helpers.MVVM;
+using AppShopping.Services;
+using System;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Xamarin.Forms;
+using ZXing.Net.Mobile.Forms;
+
+namespace AppShopping.ViewModels
+{
+    public class TicketScanViewModel : BaseViewModel
+    {
+        public string TicketNumber { get; set; }
+        public ICommand TicketScanCommand { get; set; }
+        public ICommand TicketPaidHistoryCommand { get; set; }
+        private string _message;
+        public string Message
+        {
+            get
+            {
+                return _message;
+            }
+            set
+            {
+                SetProperty(ref _message, value);
+            }
+        }
+
+        public TicketScanViewModel()
+        {
+            TicketScanCommand = new MvvmHelpers.Commands.AsyncCommand(TicketScan);
+            TicketPaidHistoryCommand = new Command(TicketPaidHistory);
+        }
+
+        private async Task TicketScan()
+        {
+            var scanPage = new ZXingScannerPage();
+            scanPage.OnScanResult += async (result) =>
+            {
+                scanPage.IsScanning = false;
+
+                await Shell.Current.Navigation.PopAsync();
+
+                Message = result.Text;
+
+                TicketProccess(result.Text);
+
+            };
+
+            await Shell.Current.Navigation.PushAsync(scanPage);
+
+            //TicketProccess("");
+        }
+        private void TicketPaidHistory()
+        {
+
+        }
+        private void TicketProccess(string ticketNumber)
+        {
+            try
+            {
+                var ticket = new TicketService().GetTicket(ticketNumber);
+
+            }
+            catch (Exception e)
+            {
+
+                Message = e.Message;
+            }
+        }
+    }
+}
